7db8f7e516bf5f23efc266358a098852d84253f3
ZTestJohn <ZTestJohn@users.noreply.github.com>
2024-05-04 19:04:58 -0300

    update

--- Geografia/script.js
+++ Geografia/script.js
@@ -1,1 +1,7 @@
-ScrollReveal().reveal('.sect');
+window.addEventListener('scroll', scrollAltura())
+function scrollAltura(){
+    console.log(window.innerHeight)
+}
+for (c=1; c+=1; c>=100){
+    setTimeout(scrollAltura, 3000)
+}
