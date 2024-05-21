f77ff135f97c27900d7e9229d696c3b05936fcdd
ZTestJohn <ZTestJohn@users.noreply.github.com>
2024-05-21 12:23:44 -0300

    Update

--- Tests/script.js
+++ Tests/script.js
@@ -1,11 +1,13 @@
 function Mudar(){
     const body = document.getElementsByTagName('html')
-    body[0].style.opacity = '0'
+    body[0].style.display = 'none'
     setTimeout(function(){
         window.location.replace('html2.html')
     },400)
 }
-window.onload = function(){
-    const html = document.getElementsByClassName('html')
-    html[0].style.opacity = '1'
+function carregou(){
+    setTimeout(function (){
+        const html = document.getElementsByClassName('html')
+        html[0].style.display = 'block'
+    },600)
 }
