bd4d7c7a713f157429cbf9077801f08181580c45
ZTestJohn <ZTestJohn@users.noreply.github.com>
2024-06-30 21:53:53 -0300

    Update script.js

--- 1.Outros projetos/Clima/script.js
+++ 1.Outros projetos/Clima/script.js
@@ -1,7 +1,12 @@
+import jsonId from './api_id.json' with {type: 'json'}
+
 const nome_cidade = document.getElementById('pesquisa').value 
+let ApiID = jsonId.id
 
 async function PesquisarCidade(){
-    try{}
+    try{
+        let resposta = await fetch()
+    }
     catch(error){
         console.error(error)
     }
